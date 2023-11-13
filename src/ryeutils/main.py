3371373ff476305e9c7ff79685d08ccfba6b5abd
Remy Yang <jyang2@caltech.edu>
2023-11-13 13:13:07 -0800

    Add docstrings

--- src/ryeutils/main.py
+++ src/ryeutils/main.py
@@ -1,6 +1,7 @@
 from collections import Counter
 import numpy as np
 from itertools import groupby
+from typing import Union, List
 
 
 def np_pearson_corr(x, y):
@@ -27,7 +28,20 @@ def np_pearson_corr(x, y):
 
 
 def occurrence(x):
-    """ Maps list elements to the nth occurrence of the element value in the list."""
+    """ Maps list elements to the nth occurrence of the element value in the list.
+
+    Args:
+        x (Union[List, np.array]): Iterable with presumed consecutive repeated values.
+
+    Returns:
+        occ (Union[List, np.array]): List of nth occurrence for corresponding value in `x`
+
+    Examples:
+       integer list::
+
+             ryeutils.main.occurrence([1, 1, 2, 2, 2, 3])
+             Out[86]: [0, 1, 0, 1, 2, 0]
+    """
 
     if isinstance(x, list):
         occ = _occurrence_list(x)
@@ -62,6 +76,21 @@ def _occurrence_list(x):
 
 
 def find_runs(x):
+    """Get values and lengths of consecutive runs in list.
+
+    Args:
+        x (Union[List, np.array]): Iterable with presumed consecutive repeated values.
+
+    Returns:
+        run_values (List): values of consecutive runs
+        run_lengths (List): length of consecutive runs
+
+    Example:
+        integer array::
+
+            >>> ryeutils.main.find_runs(np.array([3, 3, 5]))
+            Out[85]: ([3, 5], [2, 1])
+    """
     run_values = []
     run_lengths = []
     groups = []
@@ -81,18 +110,19 @@ def index_stimuli(stim_list, include_trial_idx=True):
     Computes indices for list of stimuli.
 
     Example with stim_list = 'AAABBBCCCAAABBB':
-      -       stim = AAA BBB CCC AAA BBB
-      -   stim_occ = 012 012 012 345 345
-      -    run_idx = 000 111 222 333 444
-      - idx_in_run = 012 012 012 012 012
-      -    run_occ = 000 111 222 333 444
+
+    -       stim = AAA BBB CCC AAA BBB<br>
+    -   stim_occ = 012 012 012 345 345
+    -    run_idx = 000 111 222 333 444
+    - idx_in_run = 012 012 012 012 012
+    -    run_occ = 000 111 222 333 444
 
     Args:
         stim_list (List[str]): List of stimuli
         include_trial_idx ():
 
     Returns:
-        dict: contains keys ['stim', 'stim_occ', 'run_idx', 'idx_in_run', 'run_occ']
+        stim_idx (dict): contains keys ['stim', 'stim_occ', 'run_idx', 'idx_in_run', 'run_occ']
 
     """
     stimrun, stimrun_len = find_runs(stim_list)
@@ -116,13 +146,37 @@ def index_stimuli(stim_list, include_trial_idx=True):
 
 
 def get_run_limits(stim_list):
+    """Given a list of stimuli with repeats, compute the location of labels for stimulus blocks.
+
+    `get_run_limits("AAABBBCCC")` returns:
+
+    - `labels = ['A', 'B', 'C']`
+    - `runs = [3, 3, 3]`
+    - `start_locs = [0, 3, 6]`
+    - `end_locs = [3, 6, 9]`
+
+    Args:
+        stim_list (List[str]): ex: [1-6ol, 1-6ol, 1-6ol, 1-5ol, 1-5ol, ...]
+
+    Returns:
+        labels (list): list of stimulus labels
+        runs (List(int)): length of consecutive runs
+        start_locs (List[int]): location of the first element in the run
+        end_locs (List[int]): location of the last element in the run
+
+    Example::
+
+        stim_list = "AAABBBCCC"
+        labels, runs, start_locs, end_locs = get_run_limits(stim_list)
+    """
     labels, runs = find_runs(stim_list)
     end_locs = np.cumsum(runs)
     start_locs = end_locs - np.array(runs)
-    return labels, runs, end_locs, start_locs
+    return labels, runs, start_locs, end_locs
 
-# def get_subplot_label_locs(label_list):
-#     stim_map = index_stimuli(label_list)
-#     stim_run_list = [(x, y) for x, y in zip(stim_map['stim'], stim_map['run_idx'])]
 
-# %%
+def get_tick_labels_and_locs(stim_list):
+    labels, runs, start_locs, end_locs = \
+        get_run_limits(stim_list)
+    tick_locs = np.vstack([start_locs, end_locs]).mean(axis=0)
+    return labels, tick_locs
